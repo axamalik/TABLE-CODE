eb070c3f77af99f2bd4b4dd7afdc4470c5719a6c
axamalik <66937106+axamalik@users.noreply.github.com>
2020-06-15 11:26:31 +0500

    Create TAB.py

---
+++ TAB.py
@@ -0,0 +1,11 @@
+n=int(input("maths marks:"))
+print(f'you enter marks {n}')
+g=int(input("physics marks:"))
+print(f'you enter marks {g}')
+u=int(input("urdu marks"))
+print(f'you enter marks {u}')
+average_of_student = (n+g+u)/3
+print(average_of_student)
+if average_of_student > 85:
+    print("your fee is exempted")
+    
